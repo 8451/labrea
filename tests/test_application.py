51c08bcb481409021950c9d3b038027dceb13f76
Austin Warner <austin.warner@8451.com>
2024-06-28 09:51:16 -0400

    Add partial application

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,4 +1,4 @@
-from labrea.application import FunctionApplication
+from labrea.application import FunctionApplication, PartialApplication
 from labrea.option import Option
 from labrea.evaluatable import KeyNotFoundError
 import pytest
@@ -56,3 +56,59 @@ def test_lift():
         return a + b
 
     assert good_deco_kwargs.evaluate({'A': 1, 'B': 2}) == 3
+
+
+def test_partial_application():
+    b = Option('B')
+
+    app = PartialApplication(add, b)
+
+    assert app.evaluate({'B': 2})(1) == 3
+    with pytest.raises(KeyNotFoundError):
+        app.evaluate({})
+
+    app.validate({'B': 2})
+    with pytest.raises(KeyNotFoundError):
+        app.validate({})
+
+    assert app.keys({'B': 2}) == {'B'}
+    with pytest.raises(KeyNotFoundError):
+        app.keys({})
+
+    assert app.explain() == {'B'}
+
+    assert repr(app) == "PartialApplication(add, Option('B'))"
+
+
+def test_lift_partial():
+    def good(a: float, b: float = Option('B')) -> float:
+        return a + b
+
+    def bad(a: float, b: float) -> float:
+        return a + b
+
+    def bad2(a: float = Option('A'), b: float = Option('B')) -> float:
+        return a + b
+
+    assert PartialApplication.lift(good).evaluate({'B': 2})(1) == 3
+
+    with pytest.raises(TypeError):
+        PartialApplication.lift(bad)
+
+    with pytest.raises(TypeError):
+        PartialApplication.lift(bad2)
+
+    assert PartialApplication.lift(bad, b=Option('B')).evaluate({'B': 2})(1) == 3
+
+    @PartialApplication.lift
+    def good_deco(a: float, b: float = Option('B')) -> float:
+        return a + b
+
+    assert good_deco.evaluate({'B': 2})(1) == 3
+
+    @PartialApplication.lift(b=Option('B'))
+    def good_deco_kwargs(a: float, b: float = Option('X')) -> float:
+        return a + b
+
+    assert good_deco_kwargs.evaluate({'B': 2})(1) == 3
+
