d8c64242b652ee8e107123801b29d87249e18f39
Austin Warner <austin.warner@8451.com>
2024-07-08 12:10:10 -0400

    Refactor cache to take evaluatable directly

--- labrea/cache.py
+++ labrea/cache.py
@@ -10,7 +10,7 @@ A = TypeVar("A")
 class CacheFailure(Exception):
     """An exception raised when a cache operation fails."""
 
-    fingerprint: bytes
+    evaluatable: Evaluatable
     options: Options
     cache: "Cache"
 
@@ -18,72 +18,70 @@ class CacheFailure(Exception):
 class CacheGetFailure(CacheFailure):
     """An exception raised when a cache get operation fails."""
 
-    def __init__(self, fingerprint: bytes, options: Options, cache: "Cache") -> None:
-        self.fingerprint = fingerprint
+    def __init__(
+        self, evaluatable: Evaluatable, options: Options, cache: "Cache"
+    ) -> None:
+        self.evaluatable = evaluatable
         self.options = options
         self.cache = cache
 
-        super().__init__(
-            f"Failed to retrieve item with fingerprint {fingerprint.hex()} from cache {cache}"
-        )
+        super().__init__(f"Failed to retrieve {evaluatable} from cache {cache}")
 
 
 class CacheSetFailure(CacheFailure):
     """An exception raised when a cache set operation fails."""
 
-    fingerprint: bytes
+    evaluatable: Evaluatable
     options: Options
     cache: "Cache"
     value: Any
 
     def __init__(
-        self, fingerprint: bytes, options: Options, cache: "Cache", value: Any
+        self, evaluatable: Evaluatable, options: Options, cache: "Cache", value: Any
     ) -> None:
-        self.fingerprint = fingerprint
+        self.evaluatable = evaluatable
         self.options = options
         self.cache = cache
         self.value = value
 
-        super().__init__(
-            f"Failed to add key {value} with fingerprint {fingerprint.hex()} in cache {cache}"
-        )
+        super().__init__(f"Failed to add value {value} from {evaluatable} to {cache}")
 
 
 class CacheExistsFailure(CacheFailure):
     """An exception raised when a cache exists operation fails."""
 
-    fingerprint: bytes
+    evaluatable: Evaluatable
     options: Options
     cache: "Cache"
 
-    def __init__(self, fingerprint: bytes, options: Options, cache: "Cache") -> None:
-        self.fingerprint = fingerprint
+    def __init__(
+        self, evaluatable: Evaluatable, options: Options, cache: "Cache"
+    ) -> None:
+        self.evaluatable = evaluatable
         self.options = options
         self.cache = cache
 
-        super().__init__(
-            f"Failed to check if item with fingerprint {fingerprint.hex()} exists in cache {cache}"
-        )
+        super().__init__(f"Failed to check if {evaluatable} exists in cache {cache}")
 
 
 class Cache(Generic[A], ABC):
     """A class representing a cache of values that can be evaluated."""
 
     @abstractmethod
-    def get(self, fingerprint: bytes, options: Options) -> A:
+    def get(self, evaluatable: Evaluatable[A], options: Options) -> A:
         """Get a key from the cache.
 
         Arguments
         ---------
-        fingerprint : bytes
-            The fingerprint of the key to get.
+        evaluatable: Evaluatable
+            The evaluatable to get from the cache.
         options : Options
             The options dictionary to evaluate against.
 
         Returns
         -------
         A
-            The key from the cache.
+            The value from the cache.
 
         Raises
         ------
@@ -93,17 +91,17 @@ class Cache(Generic[A], ABC):
         raise NotImplementedError  # pragma: nocover
 
     @abstractmethod
-    def set(self, fingerprint: bytes, options: Options, value: A) -> None:
+    def set(self, evaluatable: Evaluatable[A], options: Options, value: A) -> None:
         """Set a key in the cache.
 
         Arguments
         ---------
-        fingerprint : bytes
-            The fingerprint of the key to set.
+        evaluatable: Evaluatable
+            The evaluatable to set in the cache.
         options : Options
             The options dictionary to evaluate against.
         value : A
-            The key to set in the cache.
+            The value to set in the cache.
 
         Raises
         ------
@@ -112,20 +110,20 @@ class Cache(Generic[A], ABC):
         """
         raise NotImplementedError  # pragma: nocover
 
-    def exists(self, fingerprint: bytes, options: Options) -> bool:
+    def exists(self, evaluatable: Evaluatable[A], options: Options) -> bool:
         """Check if a key exists in the cache.
 
         Arguments
         ---------
-        fingerprint : bytes
-            The fingerprint of the key to check.
+        evaluatable: Evaluatable
+            The evaluatable to check in the cache.
         options : Options
             The options dictionary to evaluate against.
 
         Returns
         -------
         bool
-            Whether the key exists in the cache.
+            Whether the value exists in the cache.
 
         Raises
         ------
@@ -133,7 +131,7 @@ class Cache(Generic[A], ABC):
             If the existence of the key cannot be checked in the cache.
         """
         try:
-            self.get(fingerprint, options)
+            self.get(evaluatable, options)
             return True
         except CacheGetFailure:
             return False
@@ -142,10 +140,10 @@ class Cache(Generic[A], ABC):
 class NoCache(Cache[Any]):
     """A class representing a cache that does not store any values."""
 
-    def get(self, fingerprint: bytes, options: Options) -> Any:
-        raise CacheGetFailure(fingerprint, options, self)
+    def get(self, evaluatable: Evaluatable, options: Options) -> Any:
+        raise CacheGetFailure(evaluatable, options, self)
 
-    def set(self, fingerprint: bytes, options: Options, value: Any) -> None:
+    def set(self, evaluatable: Evaluatable, options: Options, value: Any) -> None:
         pass
 
 
@@ -157,71 +155,73 @@ class MemoryCache(Cache[A]):
     def __init__(self) -> None:
         self._cache = {}
 
-    def get(self, fingerprint: bytes, options: Options) -> A:
+    def get(self, evaluatable: Evaluatable, options: Options) -> A:
         try:
-            return self._cache[fingerprint]
+            return self._cache[evaluatable.fingerprint(options)]
         except KeyError as e:
-            raise CacheGetFailure(fingerprint, options, self) from e
+            raise CacheGetFailure(evaluatable, options, self) from e
 
-    def set(self, fingerprint: bytes, options: Options, value: A) -> None:
-        self._cache[fingerprint] = value
+    def set(self, evaluatable: Evaluatable, options: Options, value: A) -> None:
+        self._cache[evaluatable.fingerprint(options)] = value
 
-    def exists(self, fingerprint: bytes, options: Options) -> bool:
-        return fingerprint in self._cache
+    def exists(self, evaluatable: Evaluatable, options: Options) -> bool:
+        return evaluatable.fingerprint(options) in self._cache
 
 
 class CacheSetRequest(runtime.Request[A]):
-    fingerprint: bytes
+    evaluatable: Evaluatable
     options: Options
     value: A
     cache: Cache[A]
 
-    def __init__(self, fingerprint: bytes, options: Options, value: A, cache: Cache[A]):
-        self.fingerprint = fingerprint
+    def __init__(
+        self, evaluatable: Evaluatable[A], options: Options, value: A, cache: Cache[A]
+    ):
+        self.evaluatable = evaluatable
         self.options = options
         self.value = value
         self.cache = cache
 
 
 class CacheGetRequest(runtime.Request[A]):
-    fingerprint: bytes
+    evaluatable: Evaluatable[A]
     options: Options
     cache: Cache[A]
 
-    def __init__(self, fingerprint: bytes, options: Options, cache: Cache[A]):
-        self.fingerprint = fingerprint
+    def __init__(self, evaluatable: Evaluatable, options: Options, cache: Cache[A]):
+        self.evaluatable = evaluatable
         self.options = options
         self.cache = cache
 
 
 class CacheExistsRequest(runtime.Request[bool]):
-    fingerprint: bytes
+    evaluatable: Evaluatable
     options: Options
     cache: Cache
 
-    def __init__(self, fingerprint: bytes, options: Options, cache: Cache):
-        self.fingerprint = fingerprint
+    def __init__(self, evaluatable: Evaluatable, options: Options, cache: Cache):
+        self.evaluatable = evaluatable
         self.options = options
         self.cache = cache
 
 
 @CacheSetRequest.handle
 def set_cache_handler(request: CacheSetRequest[A]) -> A:
-    request.cache.set(request.fingerprint, request.options, request.value)
+    request.cache.set(request.evaluatable, request.options, request.value)
     try:
-        return request.cache.get(request.fingerprint, request.options)
+        return request.cache.get(request.evaluatable, request.options)
     except CacheGetFailure:
         return request.value
 
 
 @CacheGetRequest.handle
 def get_cache_handler(request: CacheGetRequest[A]) -> A:
-    return request.cache.get(request.fingerprint, request.options)
+    return request.cache.get(request.evaluatable, request.options)
 
 
 @CacheExistsRequest.handle
 def exists_cache_handler(request: CacheExistsRequest) -> bool:
-    return request.cache.exists(request.fingerprint, request.options)
+    return request.cache.exists(request.evaluatable, request.options)
 
 
 def _disabled_set_cache_handler(request: CacheSetRequest[A]) -> A:
@@ -229,7 +229,7 @@ def _disabled_set_cache_handler(request: CacheSetRequest[A]) -> A:
 
 
 def _disabled_get_cache_handler(request: CacheGetRequest[A]) -> A:
-    raise CacheGetFailure(request.fingerprint, request.options, request.cache)
+    raise CacheGetFailure(request.evaluatable, request.options, request.cache)
 
 
 def _disabled_exists_cache_handler(request: CacheExistsRequest) -> bool:
@@ -257,22 +257,18 @@ class Cached(Evaluatable[A]):
         self.cache = cache
 
     def evaluate(self, options: Options) -> A:
-        fingerprint = self.evaluatable.fingerprint(options)
-
-        if CacheExistsRequest(fingerprint, options, self.cache).run():
+        if CacheExistsRequest(self.evaluatable, options, self.cache).run():
             try:
-                return CacheGetRequest(fingerprint, options, self.cache).run()
+                return CacheGetRequest(self.evaluatable, options, self.cache).run()
             except CacheGetFailure:
                 pass
 
         value = self.evaluatable.evaluate(options)
 
-        return CacheSetRequest(fingerprint, options, value, self.cache).run()
+        return CacheSetRequest(self.evaluatable, options, value, self.cache).run()
 
     def validate(self, options: Options) -> None:
-        fingerprint = self.evaluatable.fingerprint(options)
-
-        if not CacheExistsRequest(fingerprint, options, self.cache).run():
+        if not CacheExistsRequest(self.evaluatable, options, self.cache).run():
             self.evaluatable.validate(options)
 
     def keys(self, options: Options) -> Set[str]:
