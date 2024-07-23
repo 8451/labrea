d1d6d25cb96860f7f4741dc7512ef69c94e409e2
Austin Warner <austin.warner@8451.com>
2024-07-23 11:11:34 -0400

    Remove labrea.overload decorators

--- labrea/dataset.py
+++ labrea/dataset.py
@@ -36,6 +36,38 @@ Callback = MaybeEvaluatable[Callable[[A], None]]
 
 
 class Dataset(Evaluatable[A]):
+    """A class representing a dataset.
+
+    Datasets are the building blocks of Labrea programs. They represent the result of performing
+    some calculation over a set of inputs. Datasets can be composed together to create complex
+    call graphs that represent the flow of data through a program. Datasets are parameterized
+    using :code:`Options`, which allow for user input.
+
+    Datasets are not created directly; instead, they are created using the :code:`dataset`
+    decorator. This decorator takes a function that defines the dataset and returns a new
+    :code:`Dataset` object. When a dataset is evaluated, the default values to each argument
+    are evaluated using the provided options. The dataset is then evaluated using the
+    evaluated arguments.
+
+
+    Example Usage
+    -------------
+    >>> @dataset
+    ... def a_squared(a: float = Option('A')) -> float:
+    ...     return a ** 2
+    >>>
+    >>> @dataset
+    ... def b_squared(b: float = Option('B')) -> float:
+    ...     return b ** 2
+    >>>
+    >>> @dataset
+    ... def hypotenuse(a2: float = a_squared, b2: float = b_squared) -> float:
+    ...     return (a2 + b2) ** 0.5
+    >>>
+    >>> hypotenuse({'A': 3, 'B': 4})
+    5.0
+    """
+
     overloads: Overloaded[A]
     effects: List[Effect[A]]
     cache: Cache[A]
@@ -76,15 +108,19 @@ class Dataset(Evaluatable[A]):
         )
 
     def evaluate(self, options: Options) -> A:
+        """Evaluates the dataset using the provided options."""
         return self._composed.evaluate(options)
 
     def validate(self, options: Options) -> None:
+        """Validates the dataset using the provided options."""
         self._composed.validate(options)
 
     def keys(self, options: Options) -> Set[str]:
+        """Returns the option keys required by the dataset."""
         return self._composed.keys(options)
 
     def explain(self, options: Optional[Options] = None) -> Set[str]:
+        """Returns the option keys required by the dataset."""
         return self._composed.explain(options)
 
     def __repr__(self) -> str:
@@ -105,6 +141,47 @@ class Dataset(Evaluatable[A]):
         self,
         alias: Union[Hashable, List[Hashable]],
     ) -> Callable[[Callable[P, A]], FunctionApplication[P, A]]:
+        """Overloads the dataset with a new implementation. Used as a decorator.
+
+        Overloading a dataset allows you to provide a new implementation for the dataset.
+        Overloads are selected based on the dispatch value provided to the dataset. If no
+        dispatch value is provided, the default implementation is used. Datasets can also
+        be abstract, in which no default implementation is provided, and an error is raised
+        if no overload can be found.
+
+        Datasets must be created with a dispath argument in order to use overloads. If you
+        are trying to overload a dataset that you do not own (i.e. from a third-party library),
+        you can use my_dataset.set_dispatch(dispatch) to set the dispatch manually. The
+        dispatch can be any Evaluatable object that returns a hashable value, or a string
+        representing the Option key to use.
+
+        Arguments
+        ---------
+        alias : Union[Hashable, List[Hashable]]
+            The name or names to use for the overload.
+
+        Returns
+        -------
+        FunctionApplication[P, A]
+            The Evaluatable object representing the new implementation.
+
+
+        Example Usage
+        -------------
+        >>> @dataset(dispatch='INPUT.SOURCE'))
+        ... def input_data(path: str = Option('INPUT.PATH')) -> list[str]:
+        ...     with open(path, 'r') as f:
+        ...         return f.read()
+        >>>
+        >>> @input_data.overload('MOCK')
+        ... def mock_input_data() -> list[str]:
+        ...     return ['Mock', 'Data']
+        >>>
+        >>> input_data({'INPUT': {'PATH': 'data.txt'}})
+        ['Data', 'From', 'File']
+        >>> input_data({'INPUT': {'SOURCE': 'MOCK'}})
+        ['Mock', 'Data']
+        """
         if self.overloads.dispatch == Value(MISSING):
             raise ValueError(
                 "Cannot add overloads to a dataset without a dispatch. Add a dispatch by using "
@@ -113,12 +190,55 @@ class Dataset(Evaluatable[A]):
                 "my_dataset.set_dispatch(dispatch) to set the dispatch manually."
             )
 
-        return self.overloads.overload(alias)
+        if not isinstance(alias, list):
+            alias = [alias]
+
+        def decorator(func: Callable[P, A]) -> FunctionApplication[P, A]:
+            overload_ = FunctionApplication.lift(func)
+            for key in alias:
+                self.register(key, overload_)
+            return overload_
+
+        return decorator
 
     def register(self, key: Hashable, value: Evaluatable[A]) -> None:
+        """Registers a new overload for the dataset.
+
+        Registers a new overload for the dataset. Can be used if you want an overload to be
+        a pre-existing Evaluatable object, like an Option.
+
+        Arguments
+        ---------
+        key : Hashable
+            The name to use for the overload.
+        value : Evaluatable[A]
+            The Evaluatable object representing the new implementation.
+
+
+        Example Usage
+        -------------
+        >>> @dataset(dispatch='INPUT.SOURCE'))
+        ... def input_data(path: str = Option('INPUT.PATH')) -> list[str]:
+        ...     with open(path, 'r') as f:
+        ...         return f.read()
+        >>>
+        >>> input_data.register('MOCK', Value(['Mock', 'Data']))
+        """
         self.overloads.register(key, value)
 
     def set_dispatch(self, dispatch: Evaluatable[Hashable]) -> None:
+        """Sets the dispatch value for the dataset.
+
+        This is a stateful operation that changes the dispatch value for the dataset. This
+        is intended for use with third-party datasets that do not have a dispatch value set,
+        but you want to add an overload to.
+
+        Arguments
+        ---------
+        dispatch : Evaluatable[Hashable]
+            The dispatch value to use for the dataset.
+        """
+
         self.overloads = Overloaded(
             dispatch,
             self.overloads.lookup.copy(),
@@ -126,6 +246,18 @@ class Dataset(Evaluatable[A]):
         )
 
     def set_cache(self, cache: Union[Cache[A], Callable[..., Cache[A]]]) -> None:
+        """Sets the cache for the dataset.
+
+        Sets the cache for the dataset. The cache can be any object that implements the Cache
+        interface, or a callable that returns a Cache object. This is a stateful operation that
+        changes the cache for the dataset. This is intended for use with third-party datasets
+        that do not have a cache set, but you want to add a cache to.
+
+        Arguments
+        ---------
+        cache : Union[Cache[A], Callable[..., Cache[A]]]
+            The cache to use for the dataset.
+        """
         cache = cache if isinstance(cache, Cache) else cache()
         if not isinstance(cache, Cache):
             raise TypeError(f"Invalid cache: {cache}")
@@ -133,18 +265,66 @@ class Dataset(Evaluatable[A]):
         self.cache = cache
 
     def add_effect(self, effect: Union[Effect[A], Callback[A]]) -> None:
+        """Adds an effect to the dataset.
+
+        Adds an effect to the dataset. Effects are applied to the dataset when it is evaluated.
+        This is a stateful operation that changes the effects for the dataset. This is intended
+        for use with third-party datasets that you would like to add effects to (i.e. logging).
+
+        Arguments
+        ---------
+        effect : Union[Effect[A], Callback[A]]
+            The effect to add to the dataset.
+        """
         if isinstance(effect, Effect):
             self.effects.append(effect)
         else:
             self.effects.append(CallbackEffect(effect))
 
     def disable_effects(self) -> None:
+        """Disables effects for the dataset.
+
+        Disables effects for the dataset. Effects are not executed when the dataset is evaluated.
+        This is a stateful operation that changes the effects for the dataset. This is intended
+        for use with third-party datasets that you would like to disable effects for.
+        """
         self._effects_disabled = True
 
     def enable_effects(self) -> None:
+        """Enables effects for the dataset.
+
+        Enables effects for the dataset. Effects are executed when the dataset is evaluated.
+        This is a stateful operation that changes the effects for the dataset. This is intended
+        for use with third-party datasets that you would like to enable effects for.
+        """
         self._effects_disabled = False
 
     def with_options(self, options: Options) -> "Dataset[A]":
+        """Returns a new dataset with the provided options pre-set.
+
+        The provided options are pre-set, and cannot be overridden by the user. This
+        can simplify the creation of multiple datasets that have the same inputs and
+        calculations.
+
+        Arguments
+        ---------
+        options : Options
+            The options to pre-set for the dataset.
+
+        Returns
+        -------
+        Dataset[A]
+            A new dataset with the provided options pre-set.
+
+
+        Example Usage
+        -------------
+        >>> @dataset
+        ... def a_to_power(a: float = Option('A'), power: float = Option('POWER')) -> float:
+        ...     return a ** power
+        >>>
+        >>> a_squared = a_to_power.with_options({'POWER': 2})
+        """
         return Dataset(
             self.overloads,
             self.effects,
@@ -154,6 +334,22 @@ class Dataset(Evaluatable[A]):
         )
 
     def with_default_options(self, options: Options) -> "Dataset[A]":
+        """Returns a new dataset with the provided options as default.
+
+        The provided options are set as the default options for the dataset. These options
+        can be overridden by the user. This can simplify the creation of multiple datasets
+        that have the same inputs and calculations.
+
+        Arguments
+        ---------
+        options : Options
+            The options to set as the default for the dataset.
+
+        Returns
+        -------
+        Dataset[A]
+            A new dataset with the provided options as default.
+        """
         return Dataset(
             self.overloads,
             self.effects,
@@ -164,10 +360,12 @@ class Dataset(Evaluatable[A]):
 
     @property
     def default(self) -> MaybeMissing[Evaluatable[A]]:
+        """The default implementation of the dataset."""
         return self.overloads.default
 
     @property
     def is_abstract(self) -> bool:
+        """Whether the dataset is abstract (i.e. has no default implementation)."""
         return self.overloads.default is MISSING
 
 
--- labrea/overload.py
+++ labrea/overload.py
@@ -6,10 +6,9 @@ else:
     from typing import ParamSpec
 
 import threading
-from typing import Callable, Dict, Hashable, List, Optional, Set, TypeVar, Union
+from typing import Dict, Hashable, Optional, Set, TypeVar
 
 from ._missing import MISSING, MaybeMissing
-from .application import FunctionApplication
 from .conditional import switch
 from .types import Evaluatable, MaybeEvaluatable, Options
 
@@ -18,7 +17,27 @@ P = ParamSpec("P")
 
 
 class Overloaded(Evaluatable[A]):
-    """A class representing multiple implementations of an Evaluatable."""
+    """A class representing multiple implementations of an Evaluatable.
+
+    This class generalizes the idea of dataset overloads. It takes a dispatch,
+    which is an evaluatable (like an Option) that returns a key, and a lookup
+    dictionary, which maps keys to evaluatables. When the overloaded object is
+    evaluated, it uses the dispatch to determine which evaluatable to use, and
+    then evaluates that evaluatable. An optional default evaluatable can be
+    provided, which is used if the dispatch key is not found in the lookup
+    dictionary, or if the dispatch cannot be evaluated.
+
+    Arguments
+    ----------
+    dispatch : Evaluatable[Hashable]
+        The evaluatable that determines which implementation to use.
+    lookup : Dict[Hashable, Evaluatable[A]], optional
+        A dictionary mapping keys to evaluatables.
+    default : MaybeMissing[MaybeEvaluatable[A]], optional
+        The default evaluatable to use if the dispatch key is not found in the
+        lookup dictionary. If this is not provided, an exception will be raised
+        if the dispatch key is not found.
+    """
 
     dispatch: Evaluatable[Hashable]
     lookup: Dict[Hashable, Evaluatable[A]]
@@ -28,26 +47,30 @@ class Overloaded(Evaluatable[A]):
     def __init__(
         self,
         dispatch: Evaluatable[Hashable],
-        lookup: Dict[Hashable, Evaluatable[A]],
+        lookup: Optional[Dict[Hashable, Evaluatable[A]]],
         default: MaybeMissing[MaybeEvaluatable[A]] = MISSING,
     ):
         self.dispatch = dispatch
-        self.lookup = lookup
+        self.lookup = lookup or {}
         self.default = (
             Evaluatable.ensure(default) if default is not MISSING else default
         )
         self._lock = threading.Lock()
 
     def evaluate(self, options: Options) -> A:
+        """Evaluate the dispatch, and then evaluate the selected implementation."""
         return self.switch.evaluate(options)
 
     def validate(self, options: Options) -> None:
+        """Validate the dispatch and the selected implementation."""
         self.switch.validate(options)
 
     def keys(self, options: Options) -> Set[str]:
+        """Return the option keys required by the dispatch and the selected implementation.""" ""
         return self.switch.keys(options)
 
     def explain(self, options: Optional[Options] = None) -> Set[str]:
+        """Return the option keys required by the dispatch and the selected implementation."""
         return self.switch.explain(options)
 
     def __repr__(self) -> str:
@@ -57,30 +80,19 @@ class Overloaded(Evaluatable[A]):
         return f"Overloaded({self.dispatch!r}, {self.lookup!r}, {self.default!r})"
 
     def register(self, key: Hashable, value: Evaluatable[A]) -> None:
+        """Register a new implementation with the overloaded object.
+
+        Arguments
+        ----------
+        key : Hashable
+            The key for the implementation.
+        value : Evaluatable[A]
+            The implementation to register.
+        """
         with self._lock:
             self.lookup = {**self.lookup, key: value}
 
     @property
     def switch(self) -> Evaluatable[A]:
+        """Return the switch evaluatable that determines which implementation to use."""
         return switch(self.dispatch, self.lookup, default=self.default)
-
-    def overload(
-        self,
-        alias: Union[Hashable, List[Hashable]],
-    ) -> Callable[[Callable[P, A]], FunctionApplication[P, A]]:
-        aliases = alias if isinstance(alias, list) else [alias]
-
-        def decorator(func: Callable[P, A]) -> FunctionApplication[P, A]:
-            overload = FunctionApplication.lift(func)
-            for key in aliases:
-                self.register(key, overload)
-
-            return overload
-
-        return decorator
-
-
-def overloaded(
-    dispatch: Evaluatable[Hashable],
-) -> Callable[[Evaluatable[A]], Overloaded[A]]:
-    return lambda default: Overloaded(dispatch, {}, default)
