608491929362607b7b309450deac72f27904c160
Austin Warner <austin.warner@8451.com>
2024-12-13 14:25:12 -0500

    Add tests for partial, into, and flatmap

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,6 +2,12 @@ from labrea.option import Option
 import labrea.functions as lf
 
 
+def test_partial():
+    p = Option('A') >> lf.partial(lambda x, y: x + y, 1)
+
+    assert p({'A': 2}) == 3
+
+
 def test_map():
     m = Option('A') >> lf.map(str) >> list
 
@@ -20,3 +26,15 @@ def test_reduce():
 
     assert r1({'A': [1, 2, 3, 4]}) == 10
     assert r2({'A': [1, 2, 3, 4]}) == 20
+
+
+def test_into():
+    i = Option('A') >> lf.into(lf.partial(lambda x, y, z: x + y + z, z=3))
+
+    assert i({'A': [1, 2]}) == 6
+
+
+def test_flatmap():
+    f = Option('A') >> lf.flatmap(lambda x: [x, x]) >> list
+
+    assert f({'A': [1, 2]}) == [1, 1, 2, 2]
