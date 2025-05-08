c9ad21dfffafefd09005a7a76a763b86dfd19032
Austin Warner <austin.warner@8451.com>
2025-05-08 11:30:47 -0400

    Add test and type-validation install extras

--- labrea/type_validation.py
+++ labrea/type_validation.py
@@ -1,4 +1,5 @@
-from typing import Any, Type
+import warnings
+from typing import Any, Mapping, Type
 
 from .runtime import Request
 from .types import Options
@@ -27,6 +28,31 @@ class TypeValidationRequest(Request[None]):
         self.options = options
 
 
+def _labrea_type_validation_handler(request: TypeValidationRequest):
+    try:
+        import labrea_type_validation
+    except ImportError:
+        warnings.warn(
+            "labrea_type_validation is not installed; type validation will not be enforced"
+        )
+        return
+
+    with labrea_type_validation.enabled():
+        return request.run()
+
+
 @TypeValidationRequest.handle
-def _empty_handler(request: TypeValidationRequest):
-    return
+def _default_handler(request: TypeValidationRequest):
+    # Have to manually inspect options here to avoid infinite loop
+    labrea_opts = request.options.get("LABREA", {})
+    if not isinstance(labrea_opts, Mapping):
+        return
+
+    type_validation_opts = labrea_opts.get("TYPE_VALIDATION", {})
+    if not isinstance(type_validation_opts, Mapping):
+        return
+
+    if type_validation_opts.get("ENABLED", False) is not True:
+        return
+
+    return _labrea_type_validation_handler(request)
